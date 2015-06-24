c6412f0c899061b7348507a8c316a9a0af9e471f
danlurie <danjlurie@gmail.com>
2015-06-24 14:22:11 -0700

    Adding comments and dealing with cases where a scan directory has no images.

--- dicom_scraper.py
+++ dicom_scraper.py
@@ -4,43 +4,52 @@ import sys
 import pandas as pd
 import numpy as np
 
+# Define scraper function.
 def get_dcm_info(scan_dir, temp_path):
+    # Set parameters to scrape from common header fields (and how they should be labeled).
     params_dict = {'scan_date': 'AcquisitionDate','scanner_institution':'InstitutionName',
                    'scanner_manufacturer':'Manufacturer','scanner_model':'ManufacturerModelName',
                    'field_strength':'MagneticFieldStrength','sequence_name':'SequenceName',
                    'repetition_time':'RepetitionTime','echo_time':'EchoTime','flip_angle':'FlipAngle',
                    'pixel_spacing':'PixelSpacing','slice_thickness':'SliceThickness',
                    'slice_spacing':'SpacingBetweenSlices','matrix_size':'AcquisitionMatrix'}
+    # Set parameters to scrape from Siemens CSA header fields.
     csa_dict = {'n_slices':[0x19, 0x100a],'field_of_view':[0x51, 0x100c]}
+    # Get a list of image files in the scan directory.
     img_list = os.listdir(scan_dir)
-    info_dict = {'n_volumes':len(img_list)}
-    file_path= '/'.join([scan_dir, img_list[0]])
-    os.system('gunzip -c {} > {}'.format(file_path, temp_path))
-    try:
-        dcm_data = dicom.read_file(temp_path)
-    except:
-        print('......unable to load DICOM, continuing to next scan.')
-        for k in params_dict.keys():
-            info_dict[k] = 'Not Found'
-        for k in csa_dict.keys():
-            info_dict[k] = 'Not Found'
-        os.system('rm {}'.format(temp_path))
-        return info_dict
-    else:
-        # Try to get information from standard DICOM headers.
-        for k, v in params_dict.iteritems():
-            try:
-                info_dict[k] = dcm_data.get(v)
-            except:
-                info_dict[k] = 'Not Found'    
-        # Try to get information from Siemens CSA headers.
-        for k, v in csa_dict.iteritems():
-            try:
-                info_dict[k] = dcm_data[hex(csa_dict[k][0]), hex(csa_dict[k][1])].value
-            except:
+    # If there are image files in the scan directory...
+    if len(img_list) > 0:
+        # Initialize a dictionary with n_volumes as the first value.
+        info_dict = {'n_volumes':len(img_list)}
+        # Get the path to the first file in the image list.
+        file_path= '/'.join([scan_dir, img_list[0]])
+        # Unzip the compressed DICOM to the temporary image path.
+        os.system('gunzip -c {} > {}'.format(file_path, temp_path))
+        try:
+            dcm_data = dicom.read_file(temp_path)
+        except:
+            print('......unable to load DICOM, continuing to next scan.')
+            for k in params_dict.keys():
                 info_dict[k] = 'Not Found'
-        os.system('rm {}'.format(temp_path))
-        return info_dict
+            for k in csa_dict.keys():
+                info_dict[k] = 'Not Found'
+            os.system('rm {}'.format(temp_path))
+            return info_dict
+        else:
+            # Try to get information from standard DICOM headers.
+            for k, v in params_dict.iteritems():
+                try:
+                    info_dict[k] = dcm_data.get(v)
+                except:
+                    info_dict[k] = 'Not Found'    
+            # Try to get information from Siemens CSA headers.
+            for k, v in csa_dict.iteritems():
+                try:
+                    info_dict[k] = dcm_data[hex(csa_dict[k][0]), hex(csa_dict[k][1])].value
+                except:
+                    info_dict[k] = 'Not Found'
+            os.system('rm {}'.format(temp_path))
+            return info_dict
 
 list_path, data_dir, tmp_path, out_path = sys.argv[1:]
 
